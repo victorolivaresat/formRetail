a6b3f3166b4043cf79f20f6001819574b9d5a29a
Victor Olivares <132686509+victorolivaresat@users.noreply.github.com>
2024-09-13 12:07:52 -0500

    Update 130920241207

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,11 @@ const cors = require("cors");
 const path = require("path");
 const http = require("http");
 require("dotenv").config();
+const config = require("../config");
 const app = express();
 
 // Puerto de la aplicación
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 7000;
 
 // Middlewares
 app.use(morgan("dev"));
@@ -27,7 +28,7 @@ app.use((err, req, res, next) => {
 // Función para habilitar CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: config.CORS.ORIGIN,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 204,
@@ -41,7 +42,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: config.CORS.ORIGIN,
     methods: ["GET", "POST"],
     credentials: true,
   },
